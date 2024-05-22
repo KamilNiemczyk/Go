c1200e742dc0a87a133099b469f3bdd9c8a0fdd0
KamilNiemczyk <k.niemczyk.838@studms.ug.edu.pl>
2024-05-22 23:02:54 +0200

    forest

--- Forest/main.go
+++ Forest/main.go
@@ -11,21 +11,21 @@ func main() {
 	newboard := visualizeBoard(board)    //visualize board
 	fmt.Println(newboard)                //print board
 
-	procent := percentageOfForestBurned(board) //to check how many trees are burned
-	fmt.Println("Procent of burned trees: ", procent)
+	// procent := percentageOfForestBurned(board) //to check how many trees are burned
+	// fmt.Println("Procent of burned trees: ", procent)
 
-	procent2 := percentageOfTreesBeforeBurned(board) //to check how many trees were in forest before they were burned
-	fmt.Println("Procent of trees before burned: ", procent2)
+	// procent2 := percentageOfTreesBeforeBurned(board) //to check how many trees were in forest before they were burned
+	// fmt.Println("Procent of trees before burned: ", procent2)
 
-	notBurned := whichTreesNotBurned(board)
-	fmt.Println("Trees that are not burned: ", notBurned) //to check how many trees are not burned but they were in fire
+	// notBurned := whichTreesNotBurned(board)
+	// fmt.Println("Trees that are not burned: ", notBurned) //to check how many trees are not burned but they were in fire
 
-	result := multiplyBurnSimulationIncreasingProbability(10000) //to check how many trees are burned with increasing forest density
-	fmt.Println(result)
-	generateChart1(result)
+	// result := multiplyBurnSimulationIncreasingProbability(10000) //to check how many trees are burned with increasing forest density
+	// fmt.Println(result)
+	// generateChart1(result)
 
-	result2 := howManyTreesStayedSimulation(10000) //to check how many trees stayed in forest
-	fmt.Println(result2)
-	generateChart2(result2)
+	// result2 := howManyTreesStayedSimulation(10000) //to check how many trees stayed in forest
+	// fmt.Println(result2)
+	// generateChart2(result2)
 
 }
